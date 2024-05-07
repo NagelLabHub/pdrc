7cae80eaf9f42430fab1d832260e9f815240765c
ting <zhaiting96@gmail.com>
2024-05-06 23:41:53 -0400

    accomadate null input

--- R/data_standardization.R
+++ R/data_standardization.R
@@ -12,11 +12,14 @@
 #' dat_og <- data.frame(UG = c(0.05,0.08,0.10), NHEJ = c(23,16,19), HR = c(2,4,6))
 #' dataset_std <- RE_to_zscore(dat_og, c("UG"))
 
-RE_to_zscore <- function(dataset, var_inverse) {
+RE_to_zscore <- function(dataset, var_inverse = NULL) {
 
-  # Log transformation and sign inversion
+  # Log transformation
   dataset_log <- log(dataset)
-  dataset_log[, var_inverse] <- -dataset_log[, var_inverse]
+  # Sign inversion if var_inverse is provided
+  if (!is.null(var_inverse)) {
+    dataset_log[, var_inverse] <- -dataset_log[, var_inverse]
+  }
 
   mean_sd_priorscaling <- data.frame(mean = apply(dataset_log, 2, mean, na.rm=T), sd = apply(dataset_log, 2, sd, na.rm=T))
 
